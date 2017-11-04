1ea1f2333e2e06f8a8b7fd81076f2cde697a6854
conorburke <cjburke89@gmail.com>
2017-11-04 13:07:58 -0700

    fix CORS issue with api

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,8 @@ export const fetchSenators = () => async dispatch => {
     fetch('https://api.propublica.org/congress/v1/115/senate/members.json', {
         method: 'GET',
         headers: {
-          'X-API-Key': process.env.REACT_APP_PROPUBLICA_API_KEY
+          'X-API-Key': process.env.REACT_APP_PROPUBLICA_API_KEY,
+          'Access-Control-Request-Headers': '*'
         }
       }).then(response => response.json())
       .then(data => dispatch({ type: FETCH_SENATORS, payload: data.results[0].members }));    
